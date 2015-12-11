962945ab407313c7612ebc9b238aacd96bca2940
Nathan Harrington <nharrington@wasatchphotonics.com>
2015-12-11 16:55:42 -0500

    device log configuration and comments

--- tests/test_device.py
+++ tests/test_device.py
@@ -8,40 +8,25 @@ from pysideapp import custom_logging
 
 log = custom_logging.to_stdout()
 
-#import multiprocessing
-#multiprocessing.log_to_stderr(logging.DEBUG)
-
 class TestMultiprocessingDevice:
     def test_poison_pill_technique(self):
-        mp_device = device.QueueMPDevice(pytest=True)
+        """ Invalid implementations of a multiprocessing architecture
+        will cause this test to fail at different levels. See the device
+        module itself for details on these potential failures and
+        workarounds.
+        """
+        mp_device = device.QueueMPDevice(in_pytest=True)
         mp_device.create()
 
-        assert mp_device.process_started == True
+        assert mp_device.started == True
 
         sleep_dur = 0.3
         log.debug("Main process sleep: %s", sleep_dur)
         time.sleep(sleep_dur)
 
         mp_device.close()
-        assert mp_device.process_started == False
-
-class TestProcessLogs:
-    def test_object_has_log_debug(self):
-        from fastpm100 import devices
-        mp_device = devices.QueueMPDevice()
-        mp_device.create()
-
-        sleep_dur = 1
-        count = 0
-        while(1):
-            log.debug("Sleep: %s", sleep_dur)
-            time.sleep(sleep_dur)
-            count += 1
-            if count > 3:
-                break
-
-        mp_device.close()
+        assert mp_device.started == False
 
 
-    #def test_object_called_linked_in_pyside_has_log_prints(self):
+    #def test_sub_process_logs_are_received(self):
 
