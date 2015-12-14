c8c638c28a0f4e23d96d74e23bab28180314be63
nharringtonwasatch <nharrington@wasatchphotonics.com>
2015-12-14 10:20:12 -0500

    multiprocessing logs to disk linux

--- pysideapp/custom_logging.py
+++ pysideapp/custom_logging.py
@@ -6,6 +6,7 @@ application wide log configuration.
 
 import sys
 import logging
+import platform
 
 def to_stdout():
     log = logging.getLogger()
@@ -14,4 +15,46 @@ def to_stdout():
     strm.setFormatter(frmt)
     log.addHandler(strm)
     log.setLevel(logging.DEBUG)
+    return log
+
+
+def get_location():
+    """ Determine the location to store the log file. Current directory
+    on Linux, or %APPDATA% on windows - usually
+    c:\Users\Username\AppData\Roaming
+    """
+    log_dir = "./"
+
+    if "Linux" in platform.platform():
+        return log_dir
+
+    try:
+        import ctypes
+        from ctypes import wintypes, windll
+        CSIDL_COMMON_APPDATA = 35
+        _SHGetFolderPath = windll.shell32.SHGetFolderPathW
+        _SHGetFolderPath.argtypes = [wintypes.HWND,
+                                    ctypes.c_int,
+                                    wintypes.HANDLE,
+                                    wintypes.DWORD, wintypes.LPCWSTR]
+
+        path_buf = wintypes.create_unicode_buffer(wintypes.MAX_PATH)
+        result = _SHGetFolderPath(0, CSIDL_COMMON_APPDATA, 0, 0, path_buf)
+        log_dir = path_buf.value
+    except:
+        log.exception("Problem assigning log directory")
+
+    return(log_dir)
+
+def to_file_and_stdout(application_name="PySideApp"):
+    log = to_stdout()
+
+    log_dir = get_location()
+    log_dir += "/%s_log.txt" % application_name
+
+    frmt = logging.Formatter("%(asctime)s %(name)s - %(levelname)s %(message)s")
+    file_handler = logging.FileHandler(log_dir)
+    file_handler.setFormatter(frmt)
+
+    log.addHandler(file_handler)
     return log
