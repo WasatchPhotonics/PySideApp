fef41debd9312eb20a34757842e9aa91022f37b8
nharringtonwasatch <nharrington@wasatchphotonics.com>
2016-01-04 08:41:11 -0500

    view logs visible

--- tests/test_control.py
+++ tests/test_control.py
@@ -10,6 +10,15 @@ from pysideapp import control
 from pysideapp import applog
 
 class TestControl:
+    def visualization_wait(self, my_form, qtbot, timeout=1000):
+        """ Helper function that waits for a signal created by default
+        on all widgets. Use qtbot to timeout when this signal is not
+        received.
+        """
+        signal = my_form.customContextMenuRequested
+        with qtbot.wait_signal(signal, timeout=timeout):
+            my_form.show()
+
     def test_control_logs_visible_to_caplog(self, caplog, qtbot):
         main_logger = applog.MainLogger()
 
@@ -23,11 +32,16 @@ class TestControl:
         time.sleep(1)
         assert "Control startup" in caplog.text()
 
-    def visualization_wait(self, my_form, qtbot, timeout=1000):
-        """ Helper function that waits for a signal created by default
-        on all widgets. Use qtbot to timeout when this signal is not
-        received.
-        """
-        signal = my_form.customContextMenuRequested
-        with qtbot.wait_signal(signal, timeout=timeout):
-            my_form.show()
+
+    def test_view_logs_visible_to_caplog(self, caplog, qtbot):
+        main_logger = applog.MainLogger()
+
+        app_control = control.Controller(main_logger.log_queue)
+        self.visualization_wait(app_control.form, qtbot)
+
+        app_control.close()
+        time.sleep(1)
+
+        main_logger.close()
+        time.sleep(1)
+        assert "Init of BasicWindow" in caplog.text()
